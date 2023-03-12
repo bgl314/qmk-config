81bb488f98a330c3e20337077fa23fcd67612760
bgl314 <bgl314@yahoo.com>
2023-03-12 17:47:38 -0400

    Initial commit

---
+++ common_config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+//#define EE_HANDS
+
+#define TAPPING_TERM 170
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TOGGLE 4
+#define COMBO_COUNT 26
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+
